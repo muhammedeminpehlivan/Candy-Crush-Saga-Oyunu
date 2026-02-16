15aac296c4d38d6355a9b67e8667ae098670b19a
muhammedeminpehlivan <muhammedpehlvn@gmail.com>
2026-02-16 21:25:51 +0300

    Create YüksekSkorlar.cs

---
+++ B221200058 NDP ÖDEV/YüksekSkorlar.cs
@@ -0,0 +1,36 @@
+﻿using NdpOdev.Sınıflar;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NdpOdev
+{
+    public partial class YüksekSkorlar : Form
+    {
+        public YüksekSkorlar()
+        {
+            InitializeComponent();
+            YuksekSkorlarıGoster();
+        }
+
+        private void YuksekSkorlarıGoster()
+        {
+            var highScores = SkorYonetimi.EnYuksek5SkoruGetir();
+
+            listViewHighScores.Items.Clear();
+
+            foreach (var score in highScores)
+            {
+                ListViewItem item = new ListViewItem(score.OyuncuAdı);
+                item.SubItems.Add(score.Skor.ToString());
+                listViewHighScores.Items.Add(item);
+            }
+        }
+    }
+}
